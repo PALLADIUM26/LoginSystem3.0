56de0180cefc9dcf9175bb3b14e1d4980182948a
PALLADIUM26 <pranithdutta26@gmail.com>
2024-07-31 23:29:45 +0530

    Build initialization

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        GetStarted getStartedFrame = new GetStarted();
+        getStartedFrame.setVisible(true);
+        getStartedFrame.pack();
+        getStartedFrame.setLocationRelativeTo(null);
+        
+//        SignIn signInFrame = new SignIn();
+//        signInFrame.setVisible(true);
+//        signInFrame.pack();
+//        signInFrame.setLocationRelativeTo(null);
+    }
+    
+}
